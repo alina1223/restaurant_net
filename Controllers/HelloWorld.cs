05f2ec84b5d96f51c233407416a91569ce211023
Rotari Alina <alinarotari112@gmail.com>
2025-09-13 10:04:46 +0300

    Primul commit

---
+++ Controllers/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace projectASP.Controllers
+{
+    [ApiController]
+    [Route("api/helloworld")]
+    public class HelloWorld : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new {message = "Hello world"});
+        }
+    }
+}
